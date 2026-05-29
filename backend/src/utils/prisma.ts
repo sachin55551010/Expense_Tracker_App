9ea76b712d2ad7c3fb036a833e2d33156d89538f
Sachin Khatri <sachinbansal3333@gmail.com>
2026-05-29 13:05:51 +0530

    prisma import issue

--- backend/src/utils/prisma.ts
+++ backend/src/utils/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import dotenv from "dotenv";
-import { PrismaClient } from "../../generated/prisma/client.js";
+import { PrismaClient } from "../../generated/prisma/index.js";
 dotenv.config();
 
 const connectionString = process.env.DATABASE_URL;
